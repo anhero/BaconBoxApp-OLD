c86ed77f4017200f96b51c6c00b3c8fb76894657
Joe Dupuis <joe.dupuis@anhero.net>
2011-02-06 15:35:53 -0500

    RedBoxApp can compile

--- application/Loader.cpp
+++ application/Loader.cpp
@@ -1,4 +1,8 @@
 #include "Loader.h"
+#include "RedBoxEngine.h"
 using namespace RedBox;
 Loader::Loader(): firstState(NULL) {
+	firstState = new State();
+	RedBoxEngine::addState(firstState);
+	
 }
--- application/Loader.h
+++ application/Loader.h
@@ -1,11 +1,13 @@
 #ifndef __LOADER_H
 #define __LOADER_H
-
+#include "State.h"
 namespace RedBox {
 
 	class Loader  {
 	public:
 		Loader();
+	private:
+		State * firstState;
 	};
 }
 #endif
