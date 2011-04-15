ad7d354d00ae89ee525e597d0e917d8a6dd2f694
Joe Dupuis <joe.dupuis@anhero.net>
2011-04-15 16:31:25 -0400

    The main function in the qt implementation now call the RedBoxEngine::application function.

--- ide/QtCreator/QtCreator_specific/main.cpp
+++ ide/QtCreator/QtCreator_specific/main.cpp
@@ -1,9 +1,10 @@
 #include <QtGui/QApplication>
 #include <QCoreApplication>
 #include "mainwindow.h"
-
+#include <RedBox/RedBoxEngine.h>
 int main(int argc, char *argv[])
 {
+	RedBox::RedBoxEngine::application(argc, argv);
     QApplication a(argc, argv);
 	QCoreApplication::setApplicationName("RedBoxApp");
 	//Q_INIT_RESOURCE(Resources);
