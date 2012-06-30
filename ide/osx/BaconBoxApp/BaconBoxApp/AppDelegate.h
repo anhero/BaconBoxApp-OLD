83657a15e0b8d994289ba49d7096894f9bd1f7c6
twistedjoe <joe.dupuis@anhero.net>
2012-06-30 15:00:26 -0400

    BaconBoxApp does compile.

---
+++ ide/osx/BaconBoxApp/BaconBoxApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BaconBoxApp
+//
+//  Created by Adam Labranche on 12-02-20.
+//  Copyright (c) 2012 Anhero Inc. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
